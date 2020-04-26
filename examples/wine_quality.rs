bb37897ee788ab9a2eed62cff481f399d62feccf
blhelias <brieuc.lhelias@gmail.com>
2020-04-26 15:14:49 +0200

    uncomment example

--- examples/wine_quality.rs
+++ examples/wine_quality.rs
@@ -11,17 +11,17 @@ fn main() {
     println!("{:?}", dataset);
     println!("{:?}", dataset.get_tensor(RowType::Train, ColumnType::Target));
 
-    // let mut model = Sequential::new();
+    let mut model = Sequential::new();
 
-    // let hidden_layer_1 = Layer::new(Activation::relu(), 11, 0.0);
-    // // let hidden_layer_2 = Layer::new(Activation::tanh(), 80, 0.0);
-    // let output_layer = Layer::new(Activation::relu(), 1, 0.0);
+    let hidden_layer_1 = Layer::new(Activation::relu(), 11, 0.0);
+    // let hidden_layer_2 = Layer::new(Activation::tanh(), 80, 0.0);
+    let output_layer = Layer::new(Activation::relu(), 1, 0.0);
 
-    // model.set_seed(0);
+    model.set_seed(0);
 
-    // model.add(hidden_layer_1);
-    // // model.add(hidden_layer_2);
-    // model.add(output_layer);
+    model.add(hidden_layer_1);
+    // model.add(hidden_layer_2);
+    model.add(output_layer);
 
-    // model.fit(&dataset, 2_000, true);
+    model.fit(&dataset, 2_000, true);
 }
